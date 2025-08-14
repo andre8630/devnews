01e77e3cb8814f5cce609fd251535cd063770f5a
Andre Luiz <andre8630@gmail.com>
2025-08-14 19:52:27 +0000

    add database.js e vaiarveis de ambiente

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  return response.status(200).json({ message: "hello" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  return response.status(200).json({ message: "ola" });
 }
 
 export default status;
