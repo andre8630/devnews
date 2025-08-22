9e8f418123f2c0631c6e984aad9a7b11fb0cfe2a
andre8630 <andre8630@gmail.com>
2025-08-22 15:30:59 -0300

    fix endpoint /migrations updates

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,37 +3,44 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-  const migrationsValues = {
-    dbClient: dbClient,
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
-    dir: join(process.cwd(), "infra", "migrations"), // <-- garante compatibilidade
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+  const allowedMethods = ["POST", "GET"];
 
-  if (request.method === "GET") {
-    const penddendMigrations = await migrationRunner(migrationsValues);
-    await dbClient.end();
-    return response.status(200).json(penddendMigrations);
-  }
-
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...migrationsValues,
-      dryRun: false,
-    });
-    await dbClient.end();
-    return response.status(200).json(migratedMigrations);
-  }
-  if (!request.method === "POST" || "GET") {
-    await dbClient.end();
+  if (!allowedMethods.includes(request.method)) {
     return response
       .status(404)
       .json({ message: "End-point só aceita POST ou GET" });
   }
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const migrationsValues = {
+      dbClient: dbClient,
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join(process.cwd(), "infra", "migrations"), // <-- garante compatibilidade
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
 
-  return response.status(405);
+    if (request.method === "GET") {
+      const penddendMigrations = await migrationRunner(migrationsValues);
+
+      return response.status(200).json(penddendMigrations);
+    }
+
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...migrationsValues,
+        dryRun: false,
+      });
+
+      return response.status(200).json(migratedMigrations);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await dbClient.end();
+  }
 }
