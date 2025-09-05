331889d18ba3b885dc31588d250d52977cfca37f
andre8630 <andre8630@gmail.com>
2025-09-05 09:04:10 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,32 +6,42 @@ beforeAll(async () => {
   await database.query("drop schema public cascade ; create schema public");
 });
 
-test("GET to /api/v1/migrations", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Runner pendending migrations", () => {
+      test("For the first time", async () => {
+        const response = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        const responseBody = await response.json();
+
+        expect(response.status).toBe(200);
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+
+        const migrations = await database.query(
+          "SELECT * FROM pgmigrations ORDER BY run_on DESC",
+        );
+
+        expect(migrations.rows.length).toBeGreaterThan(0);
+      });
+
+      test("For the second time", async () => {
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        const response1Body = await response1.json();
+
+        expect(response1.status).toBe(200);
+        expect(Array.isArray(response1Body)).toBe(true);
+        expect(response1Body.length).toBe(0);
+      });
+    });
   });
-
-  expect(response.status).toBe(200);
-
-  const responseBody = await response.json();
-
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
-
-  const migrations = await database.query(
-    "SELECT * FROM pgmigrations ORDER BY run_on DESC",
-  );
-
-  expect(migrations.rows.length).toBeGreaterThan(0);
-
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-
-  expect(response1.status).toBe(200);
-
-  const response1Body = await response1.json();
-
-  expect(Array.isArray(response1Body)).toBe(true);
-  expect(response1Body.length).toBe(0);
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,16 +4,20 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  const responseBody = await response.json();
-  const parsedUpdatedAt = new Date(responseBody.update_at).toISOString();
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      const responseBody = await response.json();
+      const parsedUpdatedAt = new Date(responseBody.update_at).toISOString();
 
-  expect(response.status).toBe(200);
+      expect(response.status).toBe(200);
 
-  expect(responseBody.update_at).toEqual(parsedUpdatedAt);
+      expect(responseBody.update_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("17.5");
-  expect(responseBody.dependencies.database.max_connections).toBe(100);
-  expect(responseBody.dependencies.database.current_connections).toBe(1);
+      expect(responseBody.dependencies.database.version).toEqual("17.5");
+      expect(responseBody.dependencies.database.max_connections).toBe(100);
+      expect(responseBody.dependencies.database.current_connections).toBe(1);
+    });
+  });
 });
